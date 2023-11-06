dbd17aa818b7fc5a7986e084efaecd914227b15c
Ali Mortazavi <mortazavimaneshali@gmail.com>
2023-11-06 17:22:44 +0330

    FB

--- src/components/budgets/HtmlAndExcelExportModal.tsx
+++ src/components/budgets/HtmlAndExcelExportModal.tsx
@@ -13,7 +13,11 @@ import { useEffect, useState } from "react";
 import Select from "react-select";
 
 //Types
-import { chakraUIModalsProps, durationTypes, exportTypes } from "@/ts/types/layouts.type";
+import {
+  chakraUIModalsProps,
+  durationTypes,
+  exportTypes,
+} from "@/ts/types/layouts.type";
 
 //Redux
 import { categoriesSelector } from "@/store/category/selectors";
@@ -48,14 +52,14 @@ export default function HtmlAndExcelExportModal({
     month: string;
     day: string;
   }>({ year: "", month: "", day: "" });
-  const [duration, setDuration] = useState<durationTypes>('monthly')
-  const [exportType, setExportType] = useState<exportTypes>('excel')
+  const [duration, setDuration] = useState<durationTypes>("monthly");
+  const [exportType, setExportType] = useState<exportTypes>("excel");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   //Effects
   useEffect(() => {
     if (router.query.duration) {
-      setDuration(router.query.duration as durationTypes)
+      setDuration(router.query.duration as durationTypes);
     }
     if (router.query.category) {
       setCategory(router.query.category as string);
@@ -79,32 +83,65 @@ export default function HtmlAndExcelExportModal({
 
   //Functions
   async function exportHandler() {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      const response = await api(
-        `/budgets/${exportType}-export?duration=${duration}&year=${date.year
-        }&month=${date.month}${duration === "daily" ? "&day=" + date.day : ""
-        }${router.query.category && router.query.category != "همه دسته بندی ها"
-          ? "&category=" + router.query.category
-          : ""
-        }${router.query.date ? "&date=" + router.query.date : ""}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          responseType: "blob",
-        }
-      );
+      if (exportType == "excel") {
+        const response = await api(
+          `/budgets/excel-export?duration=${duration}&year=${date.year}&month=${
+            date.month
+          }${duration === "daily" ? "&day=" + date.day : ""}${
+            router.query.category && router.query.category != "همه دسته بندی ها"
+              ? "&category=" + router.query.category
+              : ""
+          }${router.query.date ? "&date=" + router.query.date : ""}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            responseType: "blob",
+          }
+        );
 
-      const file = new File([response.data], "test.html");
-      const url = window.URL.createObjectURL(file);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = `${exportType}-export-${new Date().getTime()}.${exportType == 'excel' ? 'xlsx' : 'html'}`;
-      link.click();
-      setIsLoading(false)
+        const file = new File(
+          [response.data],
+          `excel-export-${new Date().getTime()}.xlsx`
+        );
+        const url = window.URL.createObjectURL(file);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `excel-export-${new Date().getTime()}.xlsx`;
+        link.click();
+      } else if (exportType == "html") {
+        const response = await api(
+          `/budgets/${exportType}-export?duration=${duration}&year=${
+            date.year
+          }&month=${date.month}${
+            duration === "daily" ? "&day=" + date.day : ""
+          }${
+            router.query.category && router.query.category != "همه دسته بندی ها"
+              ? "&category=" + router.query.category
+              : ""
+          }${router.query.date ? "&date=" + router.query.date : ""}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            responseType: "blob",
+          }
+        );
+        const file = new File(
+          [response.data],
+          `html-export-${new Date().getTime()}.html`
+        );
+        const url = window.URL.createObjectURL(file);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `html-export-${new Date().getTime()}.html`;
+        link.click();
+      }
+      setIsLoading(false);
     } catch (err: any) {
-      setIsLoading(false)
+      setIsLoading(false);
       console.log(err.message);
     }
   }
@@ -135,15 +172,71 @@ export default function HtmlAndExcelExportModal({
           <FilterDatePicker date={date} setDate={setDate} />
           <hr className="border-rose-300 my-3" />
           <div className="grid grid-cols-3 items-center gap-2">
-            <div onClick={() => setDuration('daily')} className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${duration == 'daily' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none">روزانه</span></div>
-            <div onClick={() => setDuration('monthly')} className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${duration == 'monthly' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none">ماهانه</span></div>
-            <div onClick={() => setDuration('yearly')} className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${duration == 'yearly' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none">سالانه</span></div>
-            <div onClick={() => setDuration('all')} className={`col-span-3 flex items-center justify-center h-10 border border-rose-500 ${duration == 'all' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none">تمام تراکنش ها</span></div>
+            <div
+              onClick={() => setDuration("daily")}
+              className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${
+                duration == "daily"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none">روزانه</span>
+            </div>
+            <div
+              onClick={() => setDuration("monthly")}
+              className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${
+                duration == "monthly"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none">ماهانه</span>
+            </div>
+            <div
+              onClick={() => setDuration("yearly")}
+              className={`col-span-1 flex items-center justify-center h-10 border border-rose-500 ${
+                duration == "yearly"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none">سالانه</span>
+            </div>
+            <div
+              onClick={() => setDuration("all")}
+              className={`col-span-3 flex items-center justify-center h-10 border border-rose-500 ${
+                duration == "all"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none">تمام تراکنش ها</span>
+            </div>
           </div>
           <hr className="border-rose-300 my-3" />
           <div className="flex items-center gap-2">
-            <div onClick={() => setExportType('excel')} className={`flex-1 flex items-center justify-center h-10 border border-rose-500 ${exportType == 'excel' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none font-poppins text-sm">Excel</span></div>
-            <div onClick={() => setExportType('html')} className={`flex-1 flex items-center justify-center h-10 border border-rose-500 ${exportType == 'html' ? 'bg-rose-500 text-gray-100' : 'bg-transparent text-gray-800'} rounded-lg cursor-pointer`}><span className="leading-none font-poppins text-sm">Web (HTML)</span></div>
+            <div
+              onClick={() => setExportType("excel")}
+              className={`flex-1 flex items-center justify-center h-10 border border-rose-500 ${
+                exportType == "excel"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none font-poppins text-sm">Excel</span>
+            </div>
+            <div
+              onClick={() => setExportType("html")}
+              className={`flex-1 flex items-center justify-center h-10 border border-rose-500 ${
+                exportType == "html"
+                  ? "bg-rose-500 text-gray-100"
+                  : "bg-transparent text-gray-800"
+              } rounded-lg cursor-pointer`}
+            >
+              <span className="leading-none font-poppins text-sm">
+                Web (HTML)
+              </span>
+            </div>
           </div>
         </ModalBody>
 
@@ -151,7 +244,11 @@ export default function HtmlAndExcelExportModal({
           <Button colorScheme="gray" ml={3} onClick={onClose}>
             بستن
           </Button>
-          <Button isLoading={isLoading} onClick={exportHandler} colorScheme="rose">
+          <Button
+            isLoading={isLoading}
+            onClick={exportHandler}
+            colorScheme="rose"
+          >
             دریافت خروجی
           </Button>
         </ModalFooter>
